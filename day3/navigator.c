c1b69238e761d9f42200a975a2b06ce12e2848b4
Siôn Le Roux <sinisterstuf@gmail.com>
2015-12-17 01:10:45 +0100

    WIP: add implementation that would work if I understood linked lists
    Everything works fine except my segmentation fault because I fail to add
    new list items properly.  Tomorrow try either adding items to the end
    instead of start, or creating them using malloc instead of struct
    literals.

--- day3/navigator.c
+++ day3/navigator.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 typedef struct house house;
 struct house {
@@ -10,54 +9,115 @@ struct house {
     house* next;
 };
 
+void move(char, int*);
 void house_free(house*);
-int house_add(house*);
-bool house_find(int, int);
+int house_add(house**, int, int);
+house* house_find(house*, int, int);
 
 int main()
 {
     char c; // character read from stdin
-    house h = {0, 0, 0, NULL}; // fisrt house
+    house first = {0, 0, 1, NULL}; // first house
+    house* h = &first;
+    int m[2]; // movement coords
+    int count; // how many times I've visited this house
 
-    printf("%d%d%d\n", h.x, h.y, h.count);
+    printf("%d%d%d\n", h->x, h->y, h->count);
 
-    do {
-        c = fgetc(stdin);
-        putchar(c);
-    } while (c != EOF);
+    while ((c = fgetc(stdin)) != EOF) {
+        move(c, m);
+        if (m[0] + m[1] == 0) // {0, 0} Wait
+            continue;
+        count = house_add(&h, (h->x + m[0]), (h->y + m[1]));
+        printf("%c %d:%d #%d\n", c, m[0], m[1], count);
+    }
+
+    // not using heap, not needed
+    /* house_free(&h); */
 
     return 0;
 }
 
-void house_free(house *first)
-{
-    house *curr, *head = first;
-    while ((curr = head) != NULL) {
-        head = head->next;
-        free(curr);
+/**
+ * Maps arrow directions to matrix that can be added to house
+ * coordinates
+ */
+void move(char direction, int *coords) {
+    switch (direction) {
+        case '^': // North
+            coords[0] = 0;
+            coords[1] = 1;
+            break;
+        case 'v': // South
+            coords[0] = 0;
+            coords[1] = -1;
+            break;
+        case '<': // West
+            coords[0] = -1;
+            coords[1] = 0;
+            break;
+        case '>': // East
+            coords[0] = 1;
+            coords[1] = 0;
+            break;
+        default: // Wait
+            coords[0] = 0;
+            coords[1] = 0;
+            break;
     }
 }
 
-int house_add(house *first)
+// unneeded because I use only stack memory so far
+/* void house_free(house *first) */
+/* { */
+/*     house *curr, *head = first; */
+/*     while ((curr = head) != NULL) { */
+/*         head = head->next; */
+/*         free(curr); */
+/*     } */
+/* } */
+
+int house_add(house **first, int x, int y)
 {
+    puts("I'm here");
     int count = 0;
-    bool found = house_find(first->x, first->y);
+    house* f = *first;
+    house* found = house_find(f, x, y);
 
     if (found) {
-        // return early if house already visited
+        puts("found");
+        found->count++;
         return count;
     } else {
-        // allocate memory
-        // add first house to this one's next
+        puts("new");
+        // suspicious if this lets me avoid malloc: XXX: looks like this needs to be redone with malloc
+        house new = {x, y, 1, f};
+        printf("new: %d:%d %zd\n", new.x, new.y, (size_t) new.next);
         // set first pointer to this one
-        return 0;
+        *first = &new;
+        printf("updated first: %d:%d %zd.%zd\n", first.x, first.y, (size_t) first, (size_t) first.next);
+        return 1;
     }
 }
 
-bool house_find(int x, int y)
+house* house_find(house *h, int x, int y)
 {
-    // return true if x and y match an existing house
-    // possibly this doesn't need its own function
-    // possibly should return the matched house's pointer
-    return true;
+    puts("finding");
+    printf("finding %d:%d in %zd\n", x, y, (size_t) &h);
+    int c = 0;
+
+    house *i = h;
+
+    while (i != NULL) {
+        printf("checking %d:%d in %zd->%zd\n", i->x, i->y, (size_t) &i, (size_t) i->next);
+        if (i->x == x && i->y == y)
+            return i;
+
+        i = i->next;
+        c++;
+        if (c > 4)
+            break;
+    }
+
+    return NULL;
 }
