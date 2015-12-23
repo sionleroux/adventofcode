139a5ba372825eeff283df4df9ddf4bdd54b59c0
Siôn le Roux <sion.leroux@schibsted.hu>
2015-12-23 15:04:14 +0100

    Refactor to use heap instead of stack
    Also for the sake of simplicity uses a queue instead of a stack as I
    intended initially, using house_last() as a crutch.  In future it would
    be nice to refactor this as a queue even if it is technically slightly
    more difficult.

--- day3/navigator.c
+++ day3/navigator.c
@@ -9,40 +9,45 @@ struct house {
     house* next;
 };
 
-void move(char, int*);
+void arrow_to_coords(char, int*);
 void house_free(house*);
-int house_add(house**, int, int);
+house* house_init(int, int);
+house* house_add(house*, int, int);
 house* house_find(house*, int, int);
+house* house_last(house*);
+int house_count(house*);
 
 int main()
 {
-    char c; // character read from stdin
-    house first = {0, 0, 1, NULL}; // first house
-    house* h = &first;
-    int m[2]; // movement coords
-    int count; // how many times I've visited this house
 
-    printf("%d%d%d\n", h->x, h->y, h->count);
+    char c; // character read from stdin
+    int m[2]; // movement coords
+    house* h = house_init(0, 0);
+    house* l; // current house I'm at aka last house
+
+    printf("init %d:%d #%d\n", h->x, h->y, h->count);
 
     while ((c = fgetc(stdin)) != EOF) {
-        move(c, m);
+        arrow_to_coords(c, m);
         if (m[0] + m[1] == 0) // {0, 0} Wait
             continue;
-        count = house_add(&h, (h->x + m[0]), (h->y + m[1]));
-        printf("%c %d:%d #%d\n", c, m[0], m[1], count);
+        house_add(h, (h->x + m[0]), (h->y + m[1]));
+        l = house_last(h);
+        printf("%c %d:%d #%d\n", c, l->x, l->y, l->count);
     }
 
-    // not using heap, not needed
-    /* house_free(&h); */
+    printf("house count: %d\n", house_count(h));
+
+    house_free(h);
 
     return 0;
 }
 
 /**
  * Maps arrow directions to matrix that can be added to house
- * coordinates
+ * coordinates.
  */
-void move(char direction, int *coords) {
+void arrow_to_coords(char direction, int *coords) {
     switch (direction) {
         case '^': // North
             coords[0] = 0;
@@ -68,56 +73,99 @@ void move(char direction, int *coords) {
 }
 
 // unneeded because I use only stack memory so far
-/* void house_free(house *first) */
-/* { */
-/*     house *curr, *head = first; */
-/*     while ((curr = head) != NULL) { */
-/*         head = head->next; */
-/*         free(curr); */
-/*     } */
-/* } */
-
-int house_add(house **first, int x, int y)
+void house_free(house *first)
 {
-    puts("I'm here");
-    int count = 0;
-    house* f = *first;
-    house* found = house_find(f, x, y);
+    house *curr, *head = first;
+    while ((curr = head) != NULL) {
+        head = head->next;
+        free(curr);
+    }
+}
+
+house* house_init(int x, int y)
+{
+    house *h = malloc(sizeof(house));
+
+    h->count = 1;
+    h->next = NULL;
+    h->x = x;
+    h->y = y;
+
+    return h;
+}
+
+/**
+ * Adds a new house to the list of visited houses.
+ * Expects a pointer to the first house in the list and the current
+ * cooridinates.
+ * Returns a pointer to the last house.
+ */
+house* house_add(house *f, int x, int y)
+{
+    /* puts("I'm here"); */
+    house *found = house_find(f, x, y);
+    house *last;
 
     if (found) {
-        puts("found");
+        printf("found ");
         found->count++;
-        return count;
+        return found;
     } else {
-        puts("new");
-        // suspicious if this lets me avoid malloc: XXX: looks like this needs to be redone with malloc
-        house new = {x, y, 1, f};
-        printf("new: %d:%d %zd\n", new.x, new.y, (size_t) new.next);
-        // set first pointer to this one
-        *first = &new;
-        printf("updated first: %d:%d %zd.%zd\n", first.x, first.y, (size_t) first, (size_t) first.next);
-        return 1;
+        printf("new ");
+        last = house_last(f);
+        last->next = house_init(last->x + x, last->y + y);
+        /* printf("last: %d:%d %zd\n", last->x, last->y, (size_t) last->next); */
+
+        //XXX DEBUG
+        last = house_last(f);
+        /* printf("last: %d:%d %zd\n", last->x, last->y, (size_t) last->next); */
+        /* printf("updated first: %d:%d %zd.%zd\n", first.x, first.y, (size_t) first, (size_t) first.next); */
+
+        return last;
     }
 }
 
 house* house_find(house *h, int x, int y)
 {
-    puts("finding");
-    printf("finding %d:%d in %zd\n", x, y, (size_t) &h);
+    /* puts("finding"); */
+    /* printf("finding %d:%d in %zd\n", x, y, (size_t) &h); */
     int c = 0;
 
     house *i = h;
 
     while (i != NULL) {
-        printf("checking %d:%d in %zd->%zd\n", i->x, i->y, (size_t) &i, (size_t) i->next);
+        /* printf("checking %d:%d in %zd->%zd\n", i->x, i->y, (size_t) &i, (size_t) i->next); */
         if (i->x == x && i->y == y)
             return i;
 
         i = i->next;
         c++;
-        if (c > 4)
-            break;
+        /* if (c > 4) // XXX temp break stop overflow soon */
+        /*     break; */
     }
 
     return NULL;
+}
+
+house* house_last(house *h)
+{
+    house *i = h;
+
+    while (i->next != NULL)
+        i = i->next;
+
+    return i;
+}
+
+int house_count(house *h)
+{
+    house *i = h;
+    int c = 1; // number of houses
+
+    while (i->next != NULL) {
+        i = i->next;
+        c++;
+    }
+
+    return c;
 }
