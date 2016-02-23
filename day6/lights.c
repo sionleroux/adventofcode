8a6f5dc1c006edb8db501a74cc4d2dfb4a15c9db
Siôn Le Roux <sinisterstuf@gmail.com>
2016-02-23 01:32:16 +0100

    Add basic logic based on action (does nothing much though)

--- day6/lights.c
+++ day6/lights.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +15,15 @@ int main(int argc, char *argv[])
     while ((read = getline(&line, &len, stdin)) != -1) {
         sscanf(line, "%[ togleurnf] %d,%d through %d,%d", action, &from[0], &from[1], &to[0], &to[1]);
         printf("action: %s, range: %d:%d->%d:%d\n", action, from[0], from[1], to[0], to[1]);
+        if (strcmp(action, "turn on ") == 0) {
+            puts("on");
+        } else if (strcmp(action, "turn off ") == 0) {
+            puts("off");
+        } else if (strcmp(action, "toggle ") == 0) {
+            puts("toggle");
+        } else {
+            puts("bad action");
+        }
     }
     return 0;
 }
