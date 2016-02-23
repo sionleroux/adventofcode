4abe6a149bde5e8d20091784d249deb3c9991ddc
Siôn Le Roux <sinisterstuf@gmail.com>
2016-02-23 01:29:25 +0100

    Fix lights input parser
    Those .s were coordinate separators, not decimal points.  And some
    format string magic was needed to get the action to match past the first
    space.

--- day6/lights.c
+++ day6/lights.c
@@ -8,13 +8,12 @@ int main(int argc, char *argv[])
     ssize_t read;
 
     char action[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    float from;
-    float to;
+    int from[2] = {0, 0};
+    int to[2] = {0, 0};
 
     while ((read = getline(&line, &len, stdin)) != -1) {
-        sscanf(line, "%s %f through %f", action, &from, &to);
-        printf("action: %s, range: %f->%f\n", action, from, to);
+        sscanf(line, "%[ togleurnf] %d,%d through %d,%d", action, &from[0], &from[1], &to[0], &to[1]);
+        printf("action: %s, range: %d:%d->%d:%d\n", action, from[0], from[1], to[0], to[1]);
     }
-
     return 0;
 }
